df47ae27b0ac555921eee356388017b31484f926
Ian Harris <ian@fofgof.xyz>
2023-09-03 15:23:21 -0700

    correct BPW_PATH

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       host: [process.env.BPW_HOST],
       ref: 'origin/main',
       repo: 'git@github.com:harrisi/bookplayerweb.git',
-      path: 'process.env.BPW_PATH',
+      path: process.env.BPW_PATH,
       'post-deploy': 'npm i && npm run build && npm ci --omit dev',
     },
   },
