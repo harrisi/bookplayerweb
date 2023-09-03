ca38dec5a8116ddfaff2d139b65aae226b565322
Ian Harris <ian@fofgof.xyz>
2023-09-03 15:07:32 -0700

    add sign in with apple button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte'
+  export let data
+
+  const { nonce } = data
+  onMount(() => {
+    const scriptEl = document.createElement('script')
+    scriptEl.type = 'text/javascript'
+    scriptEl.src = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js'
+    // scriptEl.defer = true
+    document.body.append(scriptEl)
+  })
+</script>
+
+<svelte:head>
+  <meta name="appleid-signin-client-id" content="com.tortugapower.audiobookplayer.loginservice">
+  <meta name="appleid-signin-scope" content="email">
+  <meta name="appleid-signin-redirect-uri" content="https://bp.fofgof.xyz/auth">
+  <meta name="appleid-signin-state" content="bp.fofgof.xyz login">
+  <meta name="appleid-signin-nonce" content="{nonce}">
+  <meta name="appleid-signin-use-popup" content="true">
+</svelte:head>
+
+<h1>Welcome to BookPlayerWeb</h1>
+
+<p>This is a web front end for <a href="https://github.com/tortugapower/bookplayer">BookPlayer</a></p>
+
+<div id="appleid-signin" data-color="black" data-border="true" data-type="sign in"></div>
