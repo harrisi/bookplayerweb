18c12151d8dfe6d71ef86373d0aebe68e5bbb1b8
Ian Harris <ian@fofgof.xyz>
2023-09-04 16:32:27 -0700

    update repo and apps

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,21 @@
 module.exports = {
+  apps: [
+    {
+      name: 'bookplayerweb',
+      script: 'node build',
+      time: true,
+      watch: false,
+      env: {
+        PORT: 3001,
+      },
+    },
+  ],
   deploy: {
     production: {
       user: process.env.BPW_USER,
       host: [process.env.BPW_HOST],
       ref: 'origin/main',
-      repo: 'git@github.com:harrisi/bookplayerweb.git',
+      repo: 'https://github.com/harrisi/bookplayerweb.git',
       path: process.env.BPW_PATH,
       'post-deploy': 'npm i && npm run build && npm ci --omit dev',
     },
