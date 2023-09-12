c8b1da2accbb9fc6920d8418253a1a9823eb3543
Ian Harris <ian@fofgof.xyz>
2023-09-11 22:58:51 -0700

    center content and add nav header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let data
+  const { token } = data
+</script>
+
+<header>
+  <nav>
+    <div id="logo">BookPlayerWeb</div>
+    <div id="about">About</div>
+    {#if token}
+      <div id="profile">Profile</div>
+    {:else}
+      <div id="login">Login</div>
+    {/if}
+  </nav>
+</header>
+
+<div>
+  <slot />
+</div>
+
+<style>
+  div {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  }
+
+  nav {
+    display: grid;
+    grid-template-columns: 3fr 1fr 1fr;
+    border: 1px solid black;
+    height: 10vh;
+  }
+
+  nav > * {
+    border: 1px solid red;
+  }
+
+  :global(body) {
+    margin: 0;
+  }
+</style>
