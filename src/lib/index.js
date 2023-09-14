40d44dc009f3f6bcf909d068c637a8bdc748cddd
Ian Harris <ian@fofgof.xyz>
2023-09-14 10:19:03 -0700

    use apiCall in auth

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,7 +4,10 @@ import { dev } from '$app/environment'
 const root = dev ? 'http://localhost:5003/v1' : 'https://api.tortugapower.com/v1'
 
 const apiCall = async (method, path, args, token) => {
-  const f = fetch(`${root}${path}`, { headers: {'Authorization': `Bearer ${token}`}, method, }).then(resp => resp.json())
+  let headers = Object.create(null)
+  if (token) headers.Authorization = `Bearer ${token}`
+  if (dev) headers.Origin = 'https://bp.fofgof.xyz'
+  const f = fetch(`${root}${path}`, { headers, method, ...args }).then(resp => resp.json())
 
   return f
 }
--- src/routes/auth/+server.js
+++ src/routes/auth/+server.js
@@ -1,23 +1,12 @@
 import { json } from '@sveltejs/kit'
-import { dev } from '$app/environment'
+import { apiCall } from '$lib'
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ cookies, request }) {
   const data = await request.formData()
   const id_token = data.get('id_token')
 
-  const f = await fetch(dev ? 'http://localhost:5003/v1/user/login' : 'https://api.tortugapower.com/v1/user/login',
-    {
-      body: JSON.stringify({
-        token_id: id_token,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        'Origin': 'https://bp.fofgof.xyz'
-      },
-      method: 'POST',
-    }
-  ).then(async resp => await resp.json())
+  const f = await apiCall('POST', '/user/login', { body: JSON.stringify({token_id: id_token}) })
   .catch(error => error)
   const { token } = f
 
