d9bd1782cec2086283ecaaf54bd73eb8ef27e73f
Ian Harris <ian@fofgof.xyz>
2023-09-16 11:40:32 -0700

    fix signin

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,15 +1,46 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { dev } from '$app/environment'
 
-const root = dev ? 'http://localhost:5003/v1' : 'https://api.tortugapower.com/v1'
+const root = !dev ? 'http://localhost:5003/v1' : 'https://api.tortugapower.com/v1'
 
-const apiCall = async (method, path, args, token) => {
-  let headers = Object.create(null)
-  if (token) headers.Authorization = `Bearer ${token}`
-  if (dev) headers.Origin = 'https://bp.fofgof.xyz'
-  const f = fetch(`${root}${path}`, { headers, method, ...args }).then(resp => resp.json())
+const apiCall = async (method, path, body, token) => {
+  let headers = new Headers()
+  if (token) headers.append('Authorization', `Bearer ${token}`)
+  if (body) headers.append('Content-Type', 'application/json')
+  headers.append('Origin', 'https://bp.fofgof.xyz')
+  const opts = {headers, method}
+  if (body) {
+    opts.body = JSON.stringify(body)
+  }
+  const f = await fetch(`${root}${path}`, opts).then(resp => resp.json())
+  .catch(err => console.error(err))
 
   return f
 }
 
-export { apiCall }
+const library = Object.create(null)
+
+/**
+ * @param {Object} args
+ * @param {string} [args.relativePath]
+ * @param {boolean} [args.sign]
+ */
+library.getContent = async ({relativePath = '', sign} = {}) => {
+  // @ts-ignore
+  const searchParams = new URLSearchParams({ relativePath, sign })
+  return await apiCall('GET', `/library${searchParams.size ? '' : '?'}${searchParams.toString()}`)
+}
+
+const user = Object.create(null)
+
+/**
+ * @param {Object} args
+ * @param {string} args.id_token
+ */
+user.login = async ({id_token}) => {
+  // @ts-ignore
+  return await apiCall('POST', `/user/login`, {body: {token_id: id_token}})
+}
+
+
+export { apiCall, user, library }
--- src/routes/auth/+server.js
+++ src/routes/auth/+server.js
@@ -6,7 +6,7 @@ export async function POST({ cookies, request }) {
   const data = await request.formData()
   const id_token = data.get('id_token')
 
-  const f = await apiCall('POST', '/user/login', { body: JSON.stringify({token_id: id_token}) })
+  const f = await apiCall('POST', '/user/login', {token_id: id_token})
   .catch(error => error)
   const { token } = f
 
