36f769fa8d1d5d7f45b15a19cb3446d1a8a4ad39
Ian Harris <ian@fofgof.xyz>
2023-09-20 23:25:32 -0700

    add file item with percent completed

---
+++ src/routes/library/FileItem.svelte
@@ -0,0 +1,28 @@
+<script lang='ts'>
+  export let title: string
+  export let details: string
+  export let currentTime: number
+  export let duration: number
+  export let percentCompleted: number
+  $: percentCSS = `--percentCompleted: ${percentCompleted}%`
+</script>
+
+<span class='title'>{title}</span>
+<span class='details'>{details}</span>
+<span class='percent' style={percentCSS}></span>
+
+<style>
+  .percent {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    float: right;
+
+    width:  15px;
+    height: 15px;
+    border-radius: 50%;
+    background:
+    radial-gradient(closest-side, transparent 70%, var(--systemBackground) 70% 85%, var(--secondarySystemFill) 85% 100%),
+    conic-gradient(var(--secondarySystemFill) var(--percentCompleted), var(--tertiarySystemBackground) 0);
+  }
+</style>
