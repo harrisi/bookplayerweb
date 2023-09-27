5e383087b5ee6972936a8b75b853ffb3e3490fd5
Ian Harris <ian@fofgof.xyz>
2023-09-26 22:51:24 -0700

    remove store logs

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,15 +3,12 @@ import { writable, type StartStopNotifier, type Writable } from 'svelte/store'
 const stores = new Map<string, Writable<any>>()
 
 function createWritable<T>(key: string, init?: T, start?: StartStopNotifier<T>): void {
-  console.log(`creating store for ${key}, ${init}`)
   let w = writable(init, start)
   stores.set(key, w)
 }
 
 function getStore<T>(key: string, init?: T): Writable<any> {
-  console.log(`getStore(${key})`)
   if (!stores.has(key)) {
-    console.log(`didn't have ${key}, creating with ${init}`)
     createWritable(key, init)
   }
   return stores.get(key)!
