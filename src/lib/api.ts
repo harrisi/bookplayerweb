0f787b7a2d877ca807e5b93eee9a04c573efac46
Ian Harris <ian@fofgof.xyz>
2023-10-08 09:32:56 -0700

    add full api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,11 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { browser, dev } from '$app/environment'
+import type { HttpMethod } from '@sveltejs/kit'
+import type { Bookmark, Item } from './types'
 
 const root = dev ? 'http://localhost:5003/v1' : 'https://api.tortugapower.com/v1'
 
-const apiCall = async (method: string, path: string, body?: BodyInit | object | null, keepalive?: boolean) => {
+const raw = async (method: HttpMethod, path: string, body?: BodyInit | object | null, keepalive?: boolean) => {
   const token = browser ? localStorage.getItem('token') : ''
   let headers = new Headers()
   if (token) {
@@ -28,87 +30,109 @@ const apiCall = async (method: string, path: string, body?: BodyInit | object | 
   return f
 }
 
+const apiCall = Object.freeze({
+  get: async (path: string, keepalive?: boolean) => {
+    return raw('GET', path, null, keepalive)
+  },
+
+  put: async (path: string, body: BodyInit | object, keepalive?: boolean) => {
+    return raw('PUT', path, body, keepalive)
+  },
+
+  post: async (path: string, body: BodyInit | object, keepalive?: boolean) => {
+    return raw('POST', path, body, keepalive)
+  },
+
+  delete: async (path: string, body?: BodyInit | object | null, keepalive?: boolean) => {
+    return raw('DELETE', path, body, keepalive)
+  },
+})
+
 const library = Object.freeze({
-  getContent: async ({relativePath, sign}: {relativePath?: string, sign?: boolean}) => {
+  getContent: async ({relativePath, sign}: {relativePath?: string, sign?: boolean}, keepalive?: boolean) => {
     let path = '/library'
     let params = []
     if (relativePath) params.push(`relativePath=${encodeURIComponent(relativePath)}`)
     if (sign != undefined) params.push(`sign=${encodeURIComponent(sign)}`)
     if (params.length) path += '?' + params.join('&')
-    return await apiCall('GET', path).catch(console.error)
+    return await apiCall.get(path, keepalive).catch(console.error)
   },
 
-  getKeys: async () => {
-    return await apiCall('GET', '/library/keys').catch(console.error)
+  getKeys: async (keepalive?: boolean) => {
+    return await apiCall.get('/library/keys', keepalive)
+    .then(res => res.content)
+    .catch(console.error)
   },
 
-  createItem: async () => {
-    return await Promise.resolve('not yet implemented')
+  createItem: async (item: Item, keepalive?: boolean) => {
+    return await apiCall.put('/library', item, keepalive)
   },
 
-  updateMetadata: async (opts: {
-    relativePath: string,
-    originalFileName?: string,
-    title?: string,
-    details?: string,
-    currentTime?: number,
-    duration?: number,
-    percentCompleted?: number,
-    isFinished?: boolean,
-    orderRank?: number,
-    type?: 0 | 1 | 2,
-    lastPlayDateTimestamp?: number,
-    speed?: number,
-  }, keepAlive?: boolean) => {
-    return await apiCall('POST', '/library', opts, keepAlive).catch(console.error)
+  updateMetadata: async (metadata: Item, keepAlive?: boolean) => {
+    return await apiCall.post('/library', metadata, keepAlive).catch(console.error)
   },
 
-  moveItem: async () => {
-    return await Promise.resolve('not yet implemented')
+  moveItem: async (src: Item | string, dest: Item | string, keepalive?: boolean) => {
+    let origin = (typeof src === 'string') ? src : src.relativePath
+    let destination = (typeof dest === 'string') ? dest : dest.relativePath
+    return await apiCall.post('/library/move', { origin, destination }, keepalive)
   },
 
-  renameFolder: async () => {
-    return await Promise.resolve('not yet implemented')
+  renameFolder: async (src: Item | string, dest: Item | string, keepalive?: boolean) => {
+    let relativePath = (typeof src === 'string') ? src : src.relativePath
+    let newName = (typeof dest === 'string') ? dest : dest.relativePath
+    return await apiCall.post('/library/rename', { relativePath, newName }, keepalive)
   },
 
-  deleteItem: async () => {
-    return await Promise.resolve('not yet implemented')
+  deleteItem: async (item: Item | string, keepalive?: boolean) => {
+    let relativePath = (typeof item === 'string') ? item : item.relativePath
+    return await apiCall.delete('/library', { relativePath }, keepalive)
   },
 
-  deleteFolder: async () => {
-    return await Promise.resolve('not yet implemented')
+  deleteFolder: async (item: Item | string, keepalive?: boolean) => {
+    let relativePath = (typeof item === 'string') ? item : item.relativePath
+    return await apiCall.delete(`/library/folder_in_out?relativePath=${encodeURIComponent(relativePath)}`, null, keepalive)
   },
 
-  uploadArtwork: async () => {
-    return await Promise.resolve('not yet implemented')
+  uploadArtwork: async (item: Item | string, thumbnail: string, artwork: BodyInit, keepalive?: boolean) => {
+    let relativePath = (typeof item === 'string') ? item : item.relativePath
+    let thumbnail_name = thumbnail
+    let url = await apiCall.post('/library/thumbnail_set', { relativePath, thumbnail_name, uploaded: false }, keepalive)
+      .then(res => res.thumbnail_url)
+    await fetch(url, {
+      method: 'POST',
+      body: artwork,
+    })
+    return await apiCall.post('/library/thumbnail_set', { relativePath, thumbnail_name, uploaded: true }, keepalive)
   },
 
-  getBookmarks: async () => {
-    return await Promise.resolve('not yet implemented')
+  getBookmarks: async (item?: Item | string, keepalive?: boolean) => {
+    let relativePath = (typeof item === 'string') ? item : (item?.relativePath ?? '')
+    return await apiCall.post('/library/bookmarks', { relativePath }, keepalive)
   },
 
-  updateBookmarks: async () => {
-    return await Promise.resolve('not yet implemented')
+  updateBookmarks: async (item: Item | string, bookmark: Bookmark, keepalive?: boolean) => {
+    let key = (typeof item === 'string') ? item : (item.relativePath)
+    return await apiCall.put('/library/bookmark', { key, ...bookmark }, keepalive)
   },
 
-  getLastPlayed: async () => {
-    return await Promise.resolve('not yet implemented')
+  getLastPlayed: async (keepalive?: boolean) => {
+    return await apiCall.get('/library/last_played', keepalive).then(res => res.lastItemPlayed)
   },
 })
 
-
 const user = Object.freeze({
-  login: async ({id_token}: {id_token: string}) => {
-    return await apiCall('POST', `/user/login`, {token_id: id_token})
+  login: async ({id_token}: {id_token: string}, keepalive?: boolean) => {
+    return await apiCall.post('/user/login', {token_id: id_token}, keepalive)
   },
 
-  getInfo: async () => {
-    return await Promise.resolve('not yet implemented')
+  getInfo: async (keepalive?: boolean) => {
+    return await apiCall.get('/user', keepalive).then(res => res.user)
   },
 
-  deleteUser: async () => {
-    return await Promise.resolve('not yet implemented')
-  },
+  delete: async (keepalive?: boolean) => {
+    return await apiCall.delete('/user/delete', null, keepalive)
+  }
 })
 
 export { user, library }
