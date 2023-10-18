b5d401c8d141d0d61114b23330d46341d0c744a4
Ian Harris <ian@fofgof.xyz>
2023-10-17 22:45:02 -0700

    highlight navbar links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,9 @@
   <meta name="apple-itunes-app" content="app-id=1138219998">
   <meta name="theme-color" content="#FAFAFA" media="(prefers-color-scheme: light)">
   <meta name="theme-color" content="#202225" media="(prefers-color-scheme: dark)">
+  <link rel="preconnect" href="https://rsms.me/">
+  <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </svelte:head>
 
 <Overlay --top=5px>
@@ -22,14 +25,38 @@
     <nav>
       <div id='logoContainer'>
         <img src='logo.png' alt='bookplayer logo' />
-        <span id="logo">BookPlayerWeb</span>
+        <span id="logo">BookPlayer</span>
       </div>
-      <div id="about">About</div>
-      {#if token}
-        <a href='/' on:click={() => localStorage.removeItem('token')} id="logout">Logout</a>
-      {:else}
-        <div id="login">Login</div>
-      {/if}
+      <div id='headerControls'>
+        <div id="about">
+          <a href='?about'>
+            <span class="material-symbols-outlined">info</span>
+            About
+          </a>
+        </div>
+        <div id='library'>
+          <a href='/library'>
+            <span class='material-symbols-outlined'>library_books</span>
+            Library
+          </a>
+        </div>
+        <div id='settings'>
+          <a href='/settings'>
+            <span class='material-symbols-outlined'>settings</span>
+            Settings
+          </a>
+        </div>
+        <div id='loginout'>
+          <a href='/' on:click={() => localStorage.removeItem('token')} id="logout">
+            <span class='material-symbols-outlined'>door_open</span>
+            {#if token}
+              Logout
+            {:else}
+              <div id="login">Login</div>
+            {/if}
+          </a>
+        </div>
+      </div>
     </nav>
   </header>
 </Overlay>
@@ -41,8 +68,60 @@
 </div>
 
 <style>
+  a {
+    outline: none;
+    text-decoration: none;
+    border-radius: 3px;
+  }
+
+  a:link {
+    color: inherit;
+  }
+
+  a:visited {
+    color: inherit;
+  }
+
+  a:focus {
+    text-decoration: none;
+    background: var(--secondary);
+  }
+
+  a:hover {
+    text-decoration: none;
+    background: var(--secondary);
+  }
+
+  a:active {
+    background: var(--accent);
+    color: var(--primary);
+  }
+
+  .material-symbols-outlined {
+    font-variation-settings:
+    'FILL' var(--fill),
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 24
+  }
+
   div#container {
     display: grid;
+  }
+
+  div#headerControls {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  div#headerControls div a {
+    display: grid;
+    grid-template-rows: 1fr auto;
+    /* border: 1px solid var(--primary);
+    border-radius: 3px; */
+    padding: 5px;
+    margin: 5px;
+    justify-content: space-evenly;
   }
 
   div#logoContainer {
@@ -63,17 +142,16 @@
   }
 
   nav {
-    display: grid;
-    grid-template-columns: 3fr 1fr 1fr;
+    display: flex;
     height: 10vh;
+    justify-content: space-between;
     align-items: center;
-    justify-content: center;
     text-align: center;
   }
 
   :global(body) {
     margin: 0;
-    background-color: #F4F4F5; /* var(--systemBackground); */
+    background-color: var(--systemBackground);
     color: var(--primary);
   }
 
@@ -119,6 +197,7 @@
       --secondarySystemFill: var(--lightSecondarySystemFill);
       --tertiarySystemFill: var(--lightTertiarySystemFill);
       --quaternarySystemFill: var(--lightQuaternarySystemFill);
+      --fill: 0;
     }
   }
 
@@ -136,6 +215,12 @@
       --secondarySystemFill: var(--darkSecondarySystemFill);
       --tertiarySystemFill: var(--darkTertiarySystemFill);
       --quaternarySystemFill: var(--darkQuaternarySystemFill);
+      --fill: 0;
     }
+  }
+
+  :root { font-family: 'Inter', sans-serif; }
+  @supports (font-variation-settings: normal) {
+    :root { font-family: 'Inter var', sans-serif; }
   }
 </style>
