514f7f43131e1ceea523f4a304cc302c50d90859
Ian Harris <ian@fofgof.xyz>
2023-10-19 23:36:02 -0700

    settings styles

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,15 +1,10 @@
 <script lang='ts'>
-  import { settings } from '$lib/settings'
+  import { versioned, settings } from '$lib/settings'
   import * as devalue from 'devalue'
   import { onDestroy, onMount } from 'svelte'
 
   const onChange = () => {
-    let str = devalue.stringify($settings)
-    try {
-      localStorage.setItem('settings', str)
-    } catch (err) {
-      console.error(err)
-    }
+    versioned.settings = devalue.stringify($settings)
   }
 
   onMount(() => {
@@ -28,18 +23,17 @@
 <div id='container'>
   <h1>Settings</h1>
 
-  <h2>{$settings.general.text}</h2>
   <ul>
+    <h2>{$settings.general.text}</h2>
     <label>
       <li>
         {$settings.general.contextMenu.text}
         <input type='checkbox' bind:checked={$settings.general.contextMenu.opt} />
       </li>
     </label>
-  </ul>
 
+  <hr />
   <h2>{$settings.appearance.text}</h2>
-  <ul>
     <label>
       <li>
         {$settings.appearance.theme.text}
@@ -47,10 +41,9 @@
         <input type='text' bind:value={$settings.appearance.theme.opt} />
       </li>
     </label>
-  </ul>
 
+  <hr />
   <h2>{$settings.autoplay.text}</h2>
-  <ul>
     <label>
       <li>
         {$settings.autoplay.library.text}
@@ -63,10 +56,9 @@
         <input type='checkbox' bind:checked={$settings.autoplay.restartFinished.opt} />
       </li>
     </label>
-  </ul>
 
+  <hr />
   <h2>{$settings.playback.text}</h2>
-  <ul>
     <label>
       <li>
         {$settings.playback.boostVolume.text}
@@ -105,13 +97,14 @@
         <input type='checkbox' bind:checked={$settings.playback.smartRewind.opt} />
       </li>
     </label>
-  </ul>
 
+  <hr />
   <h2>{$settings.privacy.text}</h2>
-  <ul>
     <label>
       <li>
-        {$settings.privacy.disableCrashReports.text}
+        <p>
+          {$settings.privacy.disableCrashReports.text}
+        </p>
         <input type='checkbox' bind:checked={$settings.privacy.disableCrashReports.opt} />
       </li>
     </label>
@@ -121,5 +114,80 @@
 <style>
   #container {
     margin-left: 8px;
+    display: grid;
+    width: 70%;
+    justify-self: center;
+    font-size: 22px;
+  }
+
+  hr {
+    margin-top: 15px;
+    margin-bottom: 15px;
+    width: 100%;
+  }
+  ul {
+    list-style: none;
+    list-style-position: outside;
+    padding-left: 0;
+  }
+
+  h3 {
+    padding-left: 10px;
+  }
+
+  h3 ~ label li {
+    padding-left: 30px;
+  }
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    height: 50px;
+    padding-left: 10px;
+    background: var(--tertiarySystemBackground);
+  }
+
+  input[type='checkbox'] {
+    width: 100px;
+    height: 50px;
+    appearance: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    text-align: center;
+    line-height: 50px;
+    margin: 0;
+    color: var(--primary);
+  }
+
+  input[type='checkbox']:not(:checked)::before {
+    width: 50;
+    height: 50px;
+    appearance: none;
+    background: red;
+    content: 'x';
+  }
+
+  input[type='checkbox']:not(:checked)::after {
+    width: 50px;
+    height: 50px;
+    appearance: none;
+    content: '';
+  }
+
+  input[type='checkbox']:checked::before {
+    width: 50px;
+    height: 50px;
+    appearance: none;
+    content: '';
+  }
+
+  input[type='checkbox']:checked::after {
+    width: 50px;
+    height: 50px;
+    appearance: none;
+    background: blue;
+    content: '✓';
   }
 </style>
