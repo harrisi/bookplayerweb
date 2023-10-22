12ce4a1c5b9004ff140f0acf875c7ed4882a8e98
Ian Harris <ian@fofgof.xyz>
2023-10-21 17:02:11 -0700

    remove unused css, comments

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -132,33 +132,33 @@
     window.Buffer = Buffer
     audioEl.preservesPitch = true
     audioEl.webkitPreservesPitch = true
-    audioEl.addEventListener('audioprocess', () => console.log(`audioprocess; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('canplay', e => console.log(`canplay; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('canplaythrough', () => console.log(`canplaythrough; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('complete', () => console.log(`complete; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('durationchange', () => console.log(`durationchange; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('emptied', () => console.log(`emptied; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('audioprocess', () => console.log(`audioprocess; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('canplay', e => console.log(`canplay; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('canplaythrough', () => console.log(`canplaythrough; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('complete', () => console.log(`complete; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('durationchange', () => console.log(`durationchange; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('emptied', () => console.log(`emptied; ${currentTime}, ${audioEl.currentTime}`))
 
     audioEl.addEventListener('ended', updateMetadata)
 
-    audioEl.addEventListener('loadeddata', () => console.log(`loadeddata; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('loadedmetadata', () => { console.log(`loadedmetadata; ${currentTime}, ${audioEl.currentTime}`); loadedMetadata() })
-    audioEl.addEventListener('loadstart', () => console.log(`loadstart; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('loadeddata', () => console.log(`loadeddata; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('loadedmetadata', () => { console.log(`loadedmetadata; ${currentTime}, ${audioEl.currentTime}`); loadedMetadata() })
+    // audioEl.addEventListener('loadstart', () => console.log(`loadstart; ${currentTime}, ${audioEl.currentTime}`))
 
     audioEl.addEventListener('pause', updateMetadata)
 
-    audioEl.addEventListener('play', () => console.log(`play; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('playing', () => console.log(`playing; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('ratechange', () => console.log(`ratechange; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('seeked', () => console.log(`seeked; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('seeking', () => console.log(`seeking; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('stalled', () => console.log(`stalled; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('suspend', () => console.log(`suspend; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('play', () => console.log(`play; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('playing', () => console.log(`playing; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('ratechange', () => console.log(`ratechange; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('seeked', () => console.log(`seeked; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('seeking', () => console.log(`seeking; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('stalled', () => console.log(`stalled; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('suspend', () => console.log(`suspend; ${currentTime}, ${audioEl.currentTime}`))
 
     audioEl.addEventListener('timeupdate', () => { console.log(`timeupdate; ${currentTime}, ${audioEl.currentTime}`); $store = (audioEl.currentTime / audioEl.duration) * 100 })
 
-    audioEl.addEventListener('volumechange', () => console.log(`volumechange; ${currentTime}, ${audioEl.currentTime}`))
-    audioEl.addEventListener('waiting', () => console.log(`waiting; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('volumechange', () => console.log(`volumechange; ${currentTime}, ${audioEl.currentTime}`))
+    // audioEl.addEventListener('waiting', () => console.log(`waiting; ${currentTime}, ${audioEl.currentTime}`))
 
     if (userInteracted) {
       audioEl.play().then(() => playable = true).catch(() => playable = false)
@@ -332,121 +332,6 @@
   }
 </style>
 
-<!-- <div id='player'>
-  <p id='title'>{title}</p>
-
-  <img id='artwork' src="{thumbnail?.toString()}" alt="thumbnail for book"/>
-
-  <input id='progress' type='range' min=0 max={Math.ceil(duration)} bind:value={currentTime} />
-
-  <label id='speed'>
-    {speed}
-    <input type='range' min='0.5' max=4 step='0.1' bind:value={speed} on:change={() => audioEl.playbackRate = speed}/>
-  </label>
-
-  <button on:click={() => skip(-skipTime)}>
-    <svg id='skipReverse' viewBox='0 0 144 156'>
-      <path class='reverse'
-        d="M 58,154.44025 C 28.852681,148.15898 7.5137784,126.73372 1.5802444,97.792158 -3.4171776,73.416601 5.1212992,47.791696 23.883454,30.85778 35.229927,20.61694 47.839792,14.851346 63.587396,12.703992 l 8.32303,-1.134933 0.294787,-5.0251639 c 0.223199,-3.8048282 0.710918,-5.1042516 2.008353,-5.350837 1.71821,-0.32655597 25.709002,13.0936899 26.943914,15.0721969 1.4137,2.264944 -0.85459,4.06878 -12.38409,9.848341 -14.691884,7.364817 -16.160579,7.365183 -16.580237,0.0041 l -0.306847,-5.382271 -6.693153,0.685218 C 51.250872,22.848028 38.033046,29.265703 27.649374,39.649374 20.363481,46.935268 15.826422,54.142891 12.217015,64.165396 9.8938523,70.616292 9.5756023,73.006059 9.5756023,84 c 0,10.993941 0.31825,13.383708 2.6414127,19.8346 3.609407,10.02251 8.146466,17.23013 15.432359,24.51603 7.285894,7.28589 14.493517,11.82295 24.516022,15.43235 6.450896,2.32317 8.840663,2.64142 19.834604,2.64142 10.993941,0 13.383708,-0.31825 19.834604,-2.64142 10.022506,-3.6094 17.230126,-8.14646 24.516026,-15.43235 10.65058,-10.65059 16.97839,-23.96819 18.26348,-38.437577 C 135.34036,81.735875 136.18235,80 139.42244,80 c 4.94178,0 5.71005,4.560474 2.99732,17.792158 C 138.4493,117.15856 127.49241,133.3571 110.93527,144.33842 96.040366,154.2173 75.316862,158.17204 58,154.44025 Z"
-        id="path2" />
-      <text x='50%' y='50%' dominant-baseline='central' baseline-shift=-5 text-anchor='middle'>-{skipTime}</text>
-    </svg>
-  </button>
-
-  <button on:click={playPause} id='playPause'>play/pause</button>
-
-  <button on:click={() => skip(+skipTime)}>
-    <svg id='skip' viewBox='0 0 144 156'>
-      <path
-        d="M 58,154.44025 C 28.852681,148.15898 7.5137784,126.73372 1.5802444,97.792158 -3.4171776,73.416601 5.1212992,47.791696 23.883454,30.85778 35.229927,20.61694 47.839792,14.851346 63.587396,12.703992 l 8.32303,-1.134933 0.294787,-5.0251639 c 0.223199,-3.8048282 0.710918,-5.1042516 2.008353,-5.350837 1.71821,-0.32655597 25.709002,13.0936899 26.943914,15.0721969 1.4137,2.264944 -0.85459,4.06878 -12.38409,9.848341 -14.691884,7.364817 -16.160579,7.365183 -16.580237,0.0041 l -0.306847,-5.382271 -6.693153,0.685218 C 51.250872,22.848028 38.033046,29.265703 27.649374,39.649374 20.363481,46.935268 15.826422,54.142891 12.217015,64.165396 9.8938523,70.616292 9.5756023,73.006059 9.5756023,84 c 0,10.993941 0.31825,13.383708 2.6414127,19.8346 3.609407,10.02251 8.146466,17.23013 15.432359,24.51603 7.285894,7.28589 14.493517,11.82295 24.516022,15.43235 6.450896,2.32317 8.840663,2.64142 19.834604,2.64142 10.993941,0 13.383708,-0.31825 19.834604,-2.64142 10.022506,-3.6094 17.230126,-8.14646 24.516026,-15.43235 10.65058,-10.65059 16.97839,-23.96819 18.26348,-38.437577 C 135.34036,81.735875 136.18235,80 139.42244,80 c 4.94178,0 5.71005,4.560474 2.99732,17.792158 C 138.4493,117.15856 127.49241,133.3571 110.93527,144.33842 96.040366,154.2173 75.316862,158.17204 58,154.44025 Z"
-        id="path1" />
-      <text x='50%' y='50%' dominant-baseline='middle' baseline-shift=-5 text-anchor='middle'>+{skipTime}</text>
-    </svg>
-  </button>
-
-  <select name='sleep' id='sleep' bind:this={sleepTimerEl} bind:value={sleepTimer} on:change={sleep}>
-    {#each ['Sleep timer', '5m', '10m', '15m', '30m', '1h', 'current chapter', 'custom'] as val}
-      <option value={val}>{val}</option>
-    {/each}
-  </select>
-
-</div> -->
-
 <audio bind:this={audioEl} bind:playbackRate={speed} src={url?.toString()} bind:currentTime on:canplay={canplay}>
   <!-- <source src={url} type="audio/mp3"> -->
 </audio>
-
-<!-- <style>
-  button {
-    background-color: var(--systemBackground);
-    color: var(--primary);
-  }
-
-  img {
-    height: max(10vh, 10vw);
-    background: linear-gradient(#37398c, #537bc4);
-    border-radius: 5px;
-    aspect-ratio: 1;
-  }
-
-  #player {
-    display: grid;
-    justify-content: center;
-    align-items: center;
-    /* this can definitely be improved, but I don't want to spend too much time on it. */
-    grid-template:
-      't t t t t'
-      '. a a a .'
-      '. a a a .'
-      '. a a a .'
-      'g g g g g'
-      'e r p s l';
-  }
-
-  #sleep {
-    grid-area: l;
-  }
-
-  #speed {
-    grid-area: e;
-  }
-
-  #title {
-    grid-area: t;
-  }
-
-  #artwork {
-    grid-area: a
-  }
-
-  #progress {
-    grid-area: g;
-  }
-
-  #skipReverse {
-    grid-area: r;
-  }
-
-  #playPause {
-    height: 65px;
-    grid-area: p;
-  }
-
-  #skip {
-    grid-area: s;
-  }
-
-  svg {
-    fill: var(--primary);
-    width: 50px;
-  }
-
-  svg text {
-    font-size: 48px;
-  }
-
-  .reverse {
-    transform: scaleX(-1);
-    translate: 100%;
-  }
-</style> -->
